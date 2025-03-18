d2d9f079d23154bc6a8923f118d76c8930330743
FranMunozGlez <franciscomgz11@gmail.com>
2025-03-18 15:16:34 +0100

    feat: add routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApartmentController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('/apartment', [ApartmentController::class, 'index'])->name('apiindex');
+Route::post('/apartment', [ApartmentController::class,'store'])->name('apistore');
+Route::get('/apartment/{id}', [ApartmentController::class, 'show'])->name('apishow');
